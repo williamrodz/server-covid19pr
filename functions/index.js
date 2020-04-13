2fc7581db126bce06a666e8a2c4c5d960e21c8c9
William A Rodriguez <wrod@mit.edu>
2020-04-13 01:18:29 -0400

    Corrected ES Lint predeployment requirements. Now scraping data at 9am.

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,6 @@ admin.initializeApp({
 
 
 var Xray = require('x-ray')
-var x = Xray()
 
 const salud_web_site_url = "http://www.salud.gov.pr/Pages/coronavirus.aspx"
 const NUMBERS = "0123456789"
@@ -22,7 +21,7 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 cleanString = (text) =>{
   var output = ""
   for (var i = 0; i < text.length; i++) {
-    if (NUMBERS.indexOf(text[i]) != -1){
+    if (NUMBERS.indexOf(text[i]) !== -1){
       output += text[i]
     }
   }
@@ -50,28 +49,30 @@ getTimeStamp = ()=>{
   return {month:month,day:day,year:year,hour:hour,minutes:minutes}
 }
 
+exports.scrapeTodaysDataScheduled = functions.pubsub.schedule('5 9 * * *')
+  .timeZone('America/New_York') // Users can choose timezone - default is America/Los_Angeles
+  .onRun((context) => {
+  console.log("Scraping Today's Data:This will be run every day at 9:05 AM Eastern!");
+  var x = Xray()
 
-exports.scrape = functions.https.onRequest((request, response) => {
-
-  x(salud_web_site_url, '.ms-rteElement-H3B')(function(err, item) {
+  x(salud_web_site_url, '.ms-rteElement-H3B')((err, item) =>{
     let ref = admin.firestore().doc("data/todaysData")
     ref.update({saludTimeSignature:item})
     .then(console.log("completed updating salud time signature"))
     .catch(error=>{
       const errorMessage = "Error obtaining salud time signature\n"+error
-      console.log(errorMessage)
-      response.send(errorMessage)
+      console.log(errorMessage,err)
     })
   })
 
 
 
-  x(salud_web_site_url, ['.ms-rteElement-H2B'])(function(err, items) {
+  x(salud_web_site_url, ['.ms-rteElement-H2B'])((err, items) =>{
     integers = []
     for (var i = 0; i < items.length; i++) {
       string = items[i]
       console.log(string)
-      if (string.indexOf("COVID") == -1){// if firstChar starts with a number
+      if (string.indexOf("COVID") === -1){// if firstChar starts with a number
         integers.push(parseInt(cleanString(string)))
       }
     }
@@ -79,21 +80,23 @@ exports.scrape = functions.https.onRequest((request, response) => {
     timestamp = getTimeStamp()
     // labeledData["saludTimestampSignature"] = saludTimestampSignature
     const timeDimensions = Object.keys(timestamp)
-    for (var i = 0; i < timeDimensions.length; i++) {
-      dimension = timeDimensions[i]
+    for (var i2 = 0; i2 < timeDimensions.length; i2++) {
+      dimension = timeDimensions[i2]
       labeledData[dimension] = timestamp[dimension]
     }
 
 
     let ref = admin.firestore().doc("data/todaysData")
     ref.set(labeledData)
-    .then(response.send(labeledData))
+    .then(console.log("Wrote new entry\n",labeledData))
     .catch(error=>{
       const errorMessage = "Error scraping/writing\n"+error
-      console.log(errorMessage)
-      response.send(errorMessage)
+      console.log(errorMessage,err)
     })
   })
+
+
+  return null;
 });
 
 exports.getTodaysData = functions.https.onRequest((request, response) => {
@@ -103,7 +106,9 @@ exports.getTodaysData = functions.https.onRequest((request, response) => {
     if (snapshot.exists){
       console.log("Retrieved today's data succesfully")
       let data = snapshot.data()
-      response.send(data)
+      return response.send(data)
+    } else{
+      return response.send("Today's data does not exist")
     }
   })
   .catch(error=>{
@@ -112,8 +117,94 @@ exports.getTodaysData = functions.https.onRequest((request, response) => {
   })
 });
 
+exports.scrapeManually = functions.https.onRequest((request, response) => {
+  var x = Xray()
 
-exports.addNewDataEntry = functions.https.onRequest((request, response) => {
+  x(salud_web_site_url, '.ms-rteElement-H3B')((err, item) =>{
+    let ref = admin.firestore().doc("data/todaysData")
+    ref.update({saludTimeSignature:item})
+    .then(console.log("completed updating salud time signature"))
+    .catch(error=>{
+      const errorMessage = "Error obtaining salud time signature\n"+error
+      console.log(errorMessage,err)
+      response.send(errorMessage)
+    })
+  })
+
+
+
+  x(salud_web_site_url, ['.ms-rteElement-H2B'])((err, items)=> {
+    integers = []
+    for (var i = 0; i < items.length; i++) {
+      string = items[i]
+      console.log(string)
+      if (string.indexOf("COVID") === -1){// if firstChar starts with a number
+        integers.push(parseInt(cleanString(string)))
+      }
+    }
+    labeledData = attachLabels(integers,DATA_LABELS)
+    timestamp = getTimeStamp()
+    // labeledData["saludTimestampSignature"] = saludTimestampSignature
+    const timeDimensions = Object.keys(timestamp)
+    for (var i2 = 0; i2 < timeDimensions.length; i2++) {
+      dimension = timeDimensions[i2]
+      labeledData[dimension] = timestamp[dimension]
+    }
+
+
+    let ref = admin.firestore().doc("data/todaysData")
+    ref.set(labeledData)
+    .then(response.send(labeledData))
+    .catch(error=>{
+      const errorMessage = "Error scraping/writing\n"+error
+      console.log(errorMessage,err)
+      response.send(errorMessage)
+    })
+  })
+});
+
+
+exports.addTodaysDataToHistoryScheduled = functions.pubsub.schedule('10 9 * * *')
+  .timeZone('America/New_York') // Users can choose timezone - default is America/Los_Angeles
+  .onRun((context) => {
+  console.log("Adding Today's Data to History This will be run every day at 9:10 AM Eastern!");
+
+  let ref = admin.firestore().doc("data/todaysData")
+  ref.get()
+  .then(snapshot=>{
+    if (snapshot.exists){
+      let data = snapshot.data()
+      return data
+    } else{
+      return {noDataAvailable:true}
+    }
+  })
+  .then(newDataEntry=>{
+    let documentRef = admin.firestore().doc('data/historicalData');
+    return documentRef.update(
+      'last7Days', admin.firestore.FieldValue.arrayUnion(newDataEntry)
+    )
+  })
+    .then(data => {
+      console.log("Updated data succesfully")
+      return true
+    })
+    .catch(error=>{
+      const errorMessage = "Error updating historical data\n"+error
+      console.log(errorMessage)
+  })
+
+
+
+
+
+  return null;
+});
+
+
+
+
+exports.addTodaysDataToHistoryManually = functions.https.onRequest((request, response) => {
   let ref = admin.firestore().doc("data/todaysData")
   ref.get()
   .then(snapshot=>{
